120dabc068c310e78b224507c00f760e3d78cf45
Gustavo Ocanto <gustavoocanto@gmail.com>
2025-07-05 11:01:39 +0800

    add caddy var

--- api/schema.go
+++ api/schema.go
@@ -5,13 +5,15 @@ const dbUserNameFileName = "postgres_user"
 const dbPasswordFileName = "postgres_password"
 
 type DeploymentRequest struct {
-	SecretsDir string `validate:"required"`
-	ProjectDir string `validate:"required"`
+	SecretsDir   string `validate:"required"`
+	ProjectDir   string `validate:"required"`
+	CaddyLogsDir string `validate:"required"`
 }
 
 type Deployment struct {
 	secretsDir   string        `validate:"required"`
 	projectDir   string        `validate:"required"`
+	caddyLogsDir string        `validate:"required"`
 	dbSecrets    DbSecrets     `validate:"required"`
 	dbSecretFile DbSecretFiles `validate:"required"`
 }
