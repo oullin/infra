a45a8d57e412d6e2ba518389bd0aab860425e992
Gustavo Ocanto <gustavoocanto@gmail.com>
2025-07-06 14:08:33 +0800

    wip

--- api/schema.go
+++ api/schema.go
@@ -4,10 +4,19 @@ const dbNameFileName = "postgres_db"
 const dbUserNameFileName = "postgres_user"
 const dbPasswordFileName = "postgres_password"
 
+type DeploymentScript struct {
+	CredentialsFile string `validate:"required"`
+	FileName        string `validate:"required"`
+	Extension       string `validate:"required"`
+}
+
+//----- OLD
+
 type DeploymentRequest struct {
-	SecretsDir   string `validate:"required"`
-	ProjectDir   string `validate:"required"`
-	CaddyLogsDir string `validate:"required"`
+	SecretsDir         string `validate:"required"`
+	ProjectDir         string `validate:"required"`
+	CaddyLogsDir       string `validate:"required"`
+	ApiCredentialsFile string `validate:"required"`
 }
 
 type Deployment struct {
--- main.go
+++ main.go
@@ -5,8 +5,13 @@ import (
 	"github.com/go-playground/validator/v10"
 	"github.com/joho/godotenv"
 	"github.com/oullin/infra/api"
+	"github.com/oullin/infra/pkg"
+	"github.com/spf13/viper"
 	"log"
 	"os"
+	//"github.com/oullin/infra/api"
+	//"log"
+	//"os"
 )
 
 func main() {
@@ -17,19 +22,33 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	deployer, err := api.NewDeployment(getValidator(), api.DeploymentRequest{
-		SecretsDir:   os.Getenv("API_SECRETS_DIRECTORY"),
-		ProjectDir:   os.Getenv("API_DIRECTORY"),
-		CaddyLogsDir: os.Getenv("CADDY_LOGS_DIRECTORY"),
+	script, err := api.NewDeploymentScript(getValidator(), api.DeploymentScript{
+		FileName:        "api",
+		Extension:       "yml",
+		CredentialsFile: pkg.Trim(os.Getenv("API_CREDENTIALS_FILE")),
 	})
 
-	if err != nil {
-		log.Fatal(err)
-	}
+	//viper.SetConfigName("api")
+	//	viper.SetConfigFile("yaml")
+	//	viper.AddConfigPath(config.path)
 
-	if err = deployer.Run(); err != nil {
-		log.Fatal(err)
-	}
+	//err = viper.Rea
+
+	//
+	//deployer, err := api.NewDeployment(getValidator(), api.DeploymentRequest{
+	//	SecretsDir:         os.Getenv("API_SECRETS_DIRECTORY"),
+	//	ProjectDir:         os.Getenv("API_DIRECTORY"),
+	//	CaddyLogsDir:       os.Getenv("CADDY_LOGS_DIRECTORY"),
+	//	CredentialsFile: os.Getenv("API_CREDENTIALS_FILE"),
+	//})
+	//
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//if err = deployer.Run(); err != nil {
+	//	log.Fatal(err)
+	//}
 
 	fmt.Println("Deployment completed.")
 }
