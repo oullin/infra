e3b98f416bca404f3aeb221b3d93570552b9bdce
Gustavo Ocanto <gustavoocanto@gmail.com>
2025-07-07 15:25:39 +0800

    wip

--- api/command.go
+++ api/command.go
@@ -3,45 +3,45 @@ package api
 import "fmt"
 
 func (d *Deployment) GetCommandArgs() []string {
-    if d.Env.IsProduction() {
-        return d.GetProdCommandArgs()
-    }
+	if d.Env.IsProduction() {
+		return d.GetProdCommandArgs()
+	}
 
-    return d.GetTestingCommandArgs()
+	return d.GetTestingCommandArgs()
 }
 
 func (d *Deployment) GetProdCommandArgs() []string {
-    args := []string{
-        "-C",
-        d.Env.GetProjectRoot(),
-        d.Command,
-        fmt.Sprintf("POSTGRES_USER_SECRET_PATH=%s", d.DBSecrets.UserNameFile),
-        fmt.Sprintf("POSTGRES_PASSWORD_SECRET_PATH=%s", d.DBSecrets.PasswordFile),
-        fmt.Sprintf("POSTGRES_DB_SECRET_PATH=%s", d.DBSecrets.DbNameFile),
-        fmt.Sprintf("ENV_DB_USER_NAME=%s", d.DBSecrets.UserName),
-        fmt.Sprintf("ENV_DB_USER_PASSWORD=%s", d.DBSecrets.Password),
-        fmt.Sprintf("ENV_DB_DATABASE_NAME=%s", d.DBSecrets.DbName),
-    }
+	args := []string{
+		"-C",
+		d.Env.GetProjectRoot(),
+		d.Command,
+		fmt.Sprintf("POSTGRES_USER_SECRET_PATH=%s", d.DBSecrets.UserNameFile),
+		fmt.Sprintf("POSTGRES_PASSWORD_SECRET_PATH=%s", d.DBSecrets.PasswordFile),
+		fmt.Sprintf("POSTGRES_DB_SECRET_PATH=%s", d.DBSecrets.DbNameFile),
+		fmt.Sprintf("ENV_DB_USER_NAME=%s", d.DBSecrets.UserName),
+		fmt.Sprintf("ENV_DB_USER_PASSWORD=%s", d.DBSecrets.Password),
+		fmt.Sprintf("ENV_DB_DATABASE_NAME=%s", d.DBSecrets.DbName),
+	}
 
-    fmt.Printf("\n ---> Prod Command: %#v\n", args)
+	fmt.Printf("\n ---> Prod Command: %#v\n", args)
 
-    return args
+	return args
 }
 
 func (d *Deployment) GetTestingCommandArgs() []string {
-    args := []string{
-        "-C",
-        d.Env.GetProjectRoot(),
-        "build-test",
-        fmt.Sprintf("POSTGRES_USER_SECRET_PATH=%s", d.DBSecrets.UserNameFile),
-        fmt.Sprintf("POSTGRES_PASSWORD_SECRET_PATH=%s", d.DBSecrets.PasswordFile),
-        fmt.Sprintf("POSTGRES_DB_SECRET_PATH=%s", d.DBSecrets.DbNameFile),
-        fmt.Sprintf("ENV_DB_USER_NAME=%s", d.DBSecrets.UserName),
-        fmt.Sprintf("ENV_DB_USER_PASSWORD=%s", d.DBSecrets.Password),
-        fmt.Sprintf("ENV_DB_DATABASE_NAME=%s", d.DBSecrets.DbName),
-    }
+	args := []string{
+		"-C",
+		d.Env.GetProjectRoot(),
+		"build-test",
+		fmt.Sprintf("POSTGRES_USER_SECRET_PATH=%s", d.DBSecrets.UserNameFile),
+		fmt.Sprintf("POSTGRES_PASSWORD_SECRET_PATH=%s", d.DBSecrets.PasswordFile),
+		fmt.Sprintf("POSTGRES_DB_SECRET_PATH=%s", d.DBSecrets.DbNameFile),
+		fmt.Sprintf("ENV_DB_USER_NAME=%s", d.DBSecrets.UserName),
+		fmt.Sprintf("ENV_DB_USER_PASSWORD=%s", d.DBSecrets.Password),
+		fmt.Sprintf("ENV_DB_DATABASE_NAME=%s", d.DBSecrets.DbName),
+	}
 
-    fmt.Printf("\n ---> Test Command: %#v\n", args)
+	fmt.Printf("\n ---> Test Command: %#v\n", args)
 
-    return args
+	return args
 }
