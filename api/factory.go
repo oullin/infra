064cf262e2bf5ec806b762bd0ec9820a2943a50e
Gustavo Ocanto <gustavoocanto@gmail.com>
2025-07-07 10:57:03 +0800

    wip

--- api/factory.go
+++ api/factory.go
@@ -1,50 +1,50 @@
 package api
 
 import (
-    "fmt"
-    "github.com/go-playground/validator/v10"
-    "github.com/oullin/infra/pkg"
-    "github.com/spf13/viper"
+	"fmt"
+	"github.com/go-playground/validator/v10"
+	"github.com/oullin/infra/pkg"
+	"github.com/spf13/viper"
 )
 
 func NewDeployment(configFilePath string, validator *validator.Validate) (Deployment, error) {
-    var deployment Deployment
+	var deployment Deployment
 
-    request := DeploymentRequest{
-        Command:        DeployCommand,
-        ConfigFileName: pkg.Trim(ConfigFIleName),
-        ConfigFilePath: pkg.Trim(configFilePath),
-    }
+	request := DeploymentRequest{
+		Command:        DeployCommand,
+		ConfigFileName: pkg.Trim(ConfigFIleName),
+		ConfigFilePath: pkg.Trim(configFilePath),
+	}
 
-    if err := validator.Struct(request); err != nil {
-        return deployment, fmt.Errorf("invalid deployment request [%#v]: %v", request, err)
-    }
+	if err := validator.Struct(request); err != nil {
+		return deployment, fmt.Errorf("invalid deployment request [%#v]: %v", request, err)
+	}
 
-    viper.SetConfigName(request.ConfigFileName)
-    viper.AddConfigPath(request.ConfigFilePath)
-    viper.SetConfigType(ConfigFIleType)
+	viper.SetConfigName(request.ConfigFileName)
+	viper.AddConfigPath(request.ConfigFilePath)
+	viper.SetConfigType(ConfigFIleType)
 
-    if err := viper.ReadInConfig(); err != nil {
-        return deployment, fmt.Errorf("[api] error reading config file: %w", err)
-    }
+	if err := viper.ReadInConfig(); err != nil {
+		return deployment, fmt.Errorf("[api] error reading config file: %w", err)
+	}
 
-    deployment.DeploymentRequest = &request
-    deployment.Viper = viper.GetViper()
-    deployment.DBSecrets = nil
+	deployment.DeploymentRequest = &request
+	deployment.Viper = viper.GetViper()
+	deployment.DBSecrets = nil
 
-    if err := validator.Struct(deployment); err != nil {
-        return deployment, fmt.Errorf("invalid deployment runner [%#v]: %v", request, err)
-    }
+	if err := validator.Struct(deployment); err != nil {
+		return deployment, fmt.Errorf("invalid deployment runner [%#v]: %v", request, err)
+	}
 
-    return deployment, nil
+	return deployment, nil
 }
 
 func ParseDbCredentials(deployment Deployment) error {
-    v := deployment.Viper
-    //secrets := DBSecrets{}
+	v := deployment.Viper
+	//secrets := DBSecrets{}
 
-    fmt.Println(v.GetStringMap("database"))
-    fmt.Println(v.GetString("database.secrets.pg_dbname"))
+	fmt.Println(v.GetStringMap("database"))
+	fmt.Println(v.GetString("database.secrets.pg_dbname"))
 
-    return nil
+	return nil
 }
