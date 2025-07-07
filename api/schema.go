b1ddad71227eb0f9d1978cf47b50860d9500fb09
Gustavo Ocanto <gustavoocanto@gmail.com>
2025-07-07 16:45:47 +0800

    wip

--- api/schema.go
+++ api/schema.go
@@ -1,31 +1,31 @@
 package api
 
 import (
-	"github.com/oullin/infra/pkg"
-	"github.com/spf13/viper"
+    "github.com/oullin/infra/pkg"
+    "github.com/spf13/viper"
 )
 
 const ConfigFileName = "api"
 const ConfigFileType = "yaml"
-const DeployCommand = "build:deploy"
+const DeployCommand = "build-deploy"
 const DBNameFileName = "database.secrets.pg_dbname"
 const DBUserNameFileName = "database.secrets.pg_username"
 const DBPasswordFileName = "database.secrets.pg_password"
 
 type Deployment struct {
-	Env            *pkg.Env
-	Viper          *viper.Viper `validate:"required"`
-	DBSecrets      *DBSecrets
-	ConfigFileName string `validate:"required"`
-	ConfigFilePath string `validate:"required"`
-	Command        string `validate:"required"`
+    Env            *pkg.Env
+    Viper          *viper.Viper `validate:"required"`
+    DBSecrets      *DBSecrets
+    ConfigFileName string `validate:"required"`
+    ConfigFilePath string `validate:"required"`
+    Command        string `validate:"required"`
 }
 
 type DBSecrets struct {
-	DbName       string `validate:"required"`
-	DbNameFile   string `validate:"required"`
-	UserName     string `validate:"required"`
-	UserNameFile string `validate:"required"`
-	Password     string `validate:"required"`
-	PasswordFile string `validate:"required"`
+    DbName       string `validate:"required"`
+    DbNameFile   string `validate:"required"`
+    UserName     string `validate:"required"`
+    UserNameFile string `validate:"required"`
+    Password     string `validate:"required"`
+    PasswordFile string `validate:"required"`
 }
