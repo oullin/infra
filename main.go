867acd465fe5e9f47dcc316c171a3e7c043dbe6e
Gustavo Ocanto <gustavoocanto@gmail.com>
2025-09-08 16:34:50 +0800

    format

--- main.go
+++ main.go
@@ -1,59 +1,59 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "os"
+	"fmt"
+	"log"
+	"os"
 
-    "github.com/go-playground/validator/v10"
-    "github.com/joho/godotenv"
-    "github.com/oullin/infra/api"
-    "github.com/oullin/infra/pkg"
+	"github.com/go-playground/validator/v10"
+	"github.com/joho/godotenv"
+	"github.com/oullin/infra/api"
+	"github.com/oullin/infra/pkg"
 )
 
 var env *pkg.Env
 var validate *validator.Validate
 
 func init() {
-    if err := godotenv.Load(); err != nil {
-        panic("Error loading .env file: " + err.Error())
-    }
+	if err := godotenv.Load(); err != nil {
+		panic("Error loading .env file: " + err.Error())
+	}
 
-    validate = validator.New(
-        validator.WithRequiredStructEnabled(),
-    )
+	validate = validator.New(
+		validator.WithRequiredStructEnabled(),
+	)
 
-    wd, err := os.Getwd()
-    if err != nil {
-        panic("Error getting working directory: " + err.Error())
-    }
+	wd, err := os.Getwd()
+	if err != nil {
+		panic("Error getting working directory: " + err.Error())
+	}
 
-    env = &pkg.Env{
-        ProjectRoot:       pkg.Trim(wd),
-        AppEnv:            pkg.Trim(os.Getenv("APP_ENV")),
-        ApiProjectRoot:    pkg.Trim(os.Getenv("API_DIRECTORY")),
-        ApiConfigFilePath: pkg.Trim(os.Getenv("API_CONFIG_FILE_PATH")),
-    }
+	env = &pkg.Env{
+		ProjectRoot:       pkg.Trim(wd),
+		AppEnv:            pkg.Trim(os.Getenv("APP_ENV")),
+		ApiProjectRoot:    pkg.Trim(os.Getenv("API_DIRECTORY")),
+		ApiConfigFilePath: pkg.Trim(os.Getenv("API_CONFIG_FILE_PATH")),
+	}
 
-    if err := validate.Struct(env); err != nil {
-        panic("Invalid app env: " + err.Error())
-    }
+	if err := validate.Struct(env); err != nil {
+		panic("Invalid app env: " + err.Error())
+	}
 }
 
 func main() {
-    deployment, err := api.NewDeployment(*env, *validate)
+	deployment, err := api.NewDeployment(*env, *validate)
 
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    if err = deployment.ParseDBSecrets(); err != nil {
-        log.Fatal(err)
-    }
+	if err = deployment.ParseDBSecrets(); err != nil {
+		log.Fatal(err)
+	}
 
-    if err = deployment.Run(); err != nil {
-        log.Fatal(err)
-    }
+	if err = deployment.Run(); err != nil {
+		log.Fatal(err)
+	}
 
-    fmt.Println(pkg.MagentaColour + "\nDeployment request sent successfully ..." + pkg.Reset)
+	fmt.Println(pkg.MagentaColour + "\nDeployment request sent successfully ..." + pkg.Reset)
 }
